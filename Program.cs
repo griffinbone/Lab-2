920143dae8d5d3fddd9c5ce06d7d72ef96668632
Griffin <gtbone@crimson.ua.edu>
2020-09-09 00:07:23 -0500

    f

--- Program.cs
+++ Program.cs
@@ -4,9 +4,6 @@ public class Program
 {
 	public static void Main()
 	{
-		Console.WriteLine("Enter your height in inches: ");
-		string userInput = Console.ReadLine();
-		int number = int.Parse(userInput);
-		return number;
+		int a = 19;
 	}
 }
