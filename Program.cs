823b406e6dfc1206edf1daa2c7601898e4b0bffc
Griffin <gtbone@crimson.ua.edu>
2020-09-09 00:20:39 -0500

    f

--- Program.cs
+++ Program.cs
@@ -1,22 +1,22 @@
 using System;
 
 namespace lab2
-					
- class Program
-{
-	static void Main(string []args)
+{			
+ 	class Program
 	{
+	static void Main(string []args)
+		{
 		userInput = 5;
 		GetANumber();
 		GetANumber();
 		DisplaySum();
+		}
+
+		static int GetANumber() {
+
+			int userInput = 0;
+			Console.WriteLine("Please enter a number ");
+			userInput=Console.ReadLine();
+		}
 	}
-
-static int GetANumber() {
-
-	int userInput = 0;
-	Console.WriteLine("Please enter a number ");
-	userInput=int.Parse(Console.ReadLine());
-
-	return userInput;
-}}
+}
