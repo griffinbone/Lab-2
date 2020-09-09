0bc616711d351a25c611aa8747bfed46341752c7
Griffin <gtbone@crimson.ua.edu>
2020-09-09 01:18:21 -0500

    d

--- Program.cs
+++ Program.cs
@@ -7,35 +7,60 @@ namespace lab2
 	static void Main(string []args)
 		{
 
-			int multiply;
+	
 			int userInput;
-			int divide;
-			int StrideLength;
+			int StrideLengthFeet;
+			int StrideLengthInc;
+			int num1 = GetANumber();
+			int num2 = GetANumber();
+			int DisplayProduct; 
+			int x
+			int y
 		}
 
 		static int GetANumber() {
 
 			int userInput = 0;
-			Console.WriteLine("Please enter a number ");
+			Console.WriteLine("Please enter your height ");
 			userInput=int.Parse(Console.ReadLine());
 
 			return userInput;
 
-			Console.WriteLine("Please enter a number");
-			return int.Parse(Console.ReadLine());
+		}
+
+		static int GetANumber() {
+
+			int userInput = 0;
+			Console.WriteLine("Please enter the amount of steps you have taken ");
+			userInput=int.Parse(Console.ReadLine());
+
+			return userInput;
+		}
+
+		{
+
+		static int DisplayProduct (int num1) 
+			Console.WriteLine("The stride length in inches is: " + (num1 * 0.413 = x));
+		}
+
+		{
+		static int StrideLengthInc (int x)
+			Console.WriteLine("The stride length in feet is: " + (x / 12 = y))
 
 		}
 
-		static void DisplayProduct (int num1) 
-			Console.WriteLine("The stride length is: " + (num1 * 0.413));
+		{
+		static int StrideLengthFeet (int num2, int y)
+			Console.WriteLine("The total amount of feet walked is: " + (num2 * y = z))
 
 		}
+		{
+		static int TotalWalked (int z)
+			Console.WriteLine("The total amount of miles walked is: " + (z / 5280)
 
-
+		}
 
 	}
-
-}
 }
 
 
