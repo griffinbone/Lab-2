3e06517723de4648b6374f03c26da55d898c93da
Griffin <gtbone@crimson.ua.edu>
2020-09-08 22:31:09 -0500

    test again???

--- Program.cs
+++ Program.cs
@@ -1,16 +1,1 @@
-﻿using System;
-
-namespace lab2
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int num1 = GetNumber();
-            int num2 = GetNumber();
-
-            DisplaySum(num1, num2)
-            Console.WriteLine("This is a test");
-        }
-    }
-}
+﻿
